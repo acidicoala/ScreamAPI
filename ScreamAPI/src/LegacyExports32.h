2253b6d87bc772bb02e023f9a97e4ac8617d7c85
Acidic Koala <acidicoala@gmail.com>
2020-07-04 14:12:23 +0300

    Experimental support for older EOSSDK versions

---
+++ ScreamAPI/src/LegacyExports32.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// v1.1.0 functions
+#pragma comment(linker, "/export:EOS_AccountId_FromString=EOSSDK-Win32-Shipping_o.EOS_AccountId_FromString")
+#pragma comment(linker, "/export:EOS_AccountId_IsValid=EOSSDK-Win32-Shipping_o.EOS_AccountId_IsValid")
+#pragma comment(linker, "/export:EOS_AccountId_ToString=EOSSDK-Win32-Shipping_o.EOS_AccountId_ToString")
--- ScreamAPI/src/framework.h
+++ ScreamAPI/src/framework.h
@@ -1,5 +1,4 @@
 #pragma once
-#define NOMINMAX // Do we need this?
 #define WIN32_LEAN_AND_MEAN // Exclude not needed win api definitions
 
 #include <Windows.h>
@@ -14,8 +13,11 @@
 #include "eos-sdk/eos_ecom.h" // for dlc
 
 // Include the linker exports based on the target architecture (32 vs 64)
+// LegacyExports are special linker exports aimed to support older versions of EOS SDK
 #ifdef _WIN64
 #include "LinkerExports64.h"
+#include "LegacyExports64.h"
 #else
 #include "LinkerExports32.h"
+#include "LegacyExports32.h"
 #endif
