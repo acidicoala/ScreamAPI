aaaeba2df72af1c7f3456dd8b8656438254690b9
acidicoala <67734819+acidicoala@users.noreply.github.com>
2021-10-03 12:19:01 +0300

    Updated logger function signatures

--- src/logger/logger.hpp
+++ src/logger/logger.hpp
@@ -4,33 +4,33 @@ namespace logger {
 
     std::shared_ptr<spdlog::logger> instance();
 
-    template<typename FormatString, typename... Args>
-    void trace(const FormatString& fmt, Args&& ... args) {
+    template<typename... Args>
+    void trace(fmt::format_string<Args...> fmt, Args&& ...args) {
         instance()->trace(fmt, std::forward<Args>(args)...);
     }
 
-    template<typename FormatString, typename... Args>
-    void debug(const FormatString& fmt, Args&& ... args) {
+    template<typename... Args>
+    void debug(fmt::format_string<Args...> fmt, Args&& ...args) {
         instance()->debug(fmt, std::forward<Args>(args)...);
     }
 
-    template<typename FormatString, typename... Args>
-    void info(const FormatString& fmt, Args&& ... args) {
+    template<typename... Args>
+    void info(fmt::format_string<Args...> fmt, Args&& ...args) {
         instance()->info(fmt, std::forward<Args>(args)...);
     }
 
-    template<typename FormatString, typename... Args>
-    void warn(const FormatString& fmt, Args&& ... args) {
+    template<typename... Args>
+    void warn(fmt::format_string<Args...> fmt, Args&& ...args) {
         instance()->warn(fmt, std::forward<Args>(args)...);
     }
 
-    template<typename FormatString, typename... Args>
-    void error(const FormatString& fmt, Args&& ...args) {
+    template<typename... Args>
+    void error(fmt::format_string<Args...> fmt, Args&& ...args) {
         instance()->error(fmt, std::forward<Args>(args)...);
     }
 
-    template<typename FormatString, typename... Args>
-    void critical(const FormatString& fmt, Args&& ... args) {
+    template<typename... Args>
+    void critical(fmt::format_string<Args...> fmt, Args&& ...args) {
         instance()->critical(fmt, std::forward<Args>(args)...);
     }
 
