49d39d219317775c7a7a03b8b19dd6dd4afb3c7b
acidicoala <67734819+acidicoala@users.noreply.github.com>
2022-02-23 18:59:42 +0200

    Refactored workflow

--- build.ps1
+++ build.ps1
@@ -1,27 +1,3 @@
-# Usage:
-#   build.ps1 32 Debug
-#   build.ps1 32 Release
-#   build.ps1 64 Debug
-#   build.ps1 64 Release
-
-$arch = $args[0]
-if ($arch -notmatch '^(32|64)$') {
-    throw "Invalid architecute. Expected: '32' or '64'. Got: '$arch'"
-}
-$platform = If ($arch -eq '32') { 'Win32' } Else { 'x64' }
-
-$config = $args[1]
-if ($config -notmatch '^(Debug|Release|RelWithDebInfo)$') {
-    throw "Invalid architecute. Expected: 'Debug' or 'Release' or 'RelWithDebInfo'. Got: '$config'"
-}
-
 Set-Location (Get-Item $PSScriptRoot)
 
-$Env:BUILD_DIR = "build\$arch"
-if ($config -eq 'Debug') {
-    $Env:VERSION_SUFFIX = "-debug"
-}
-
-cmake -G "Visual Studio 17 2022" -A $platform -B "$Env:BUILD_DIR"
-
-cmake --build "$Env:BUILD_DIR" --config $config
+./KoalaBox/build.ps1 @args
